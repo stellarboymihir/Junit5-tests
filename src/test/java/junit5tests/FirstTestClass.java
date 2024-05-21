83cf16bdf233f20da11f33bd0941971b8075ee25
stellarboymihir <stellarboymihir00@gmail.com>
2024-05-21 13:30:08 +0530

    Started with Junit5

--- src/test/java/junit5tests/FirstTestClass.java
+++ src/test/java/junit5tests/FirstTestClass.java
@@ -1,2 +1,41 @@
-package junit5tests;public class FirstTestClass {
+package junit5tests;
+
+import org.junit.jupiter.api.*;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class FirstTestClass {
+
+    @BeforeAll
+    void beforeAll(){
+        System.out.println("--This is the before all method.");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("--This is the before each method.");
+    }
+
+    @AfterAll
+    void afterAll(){
+        System.out.println("--This is the after all method.");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("--This is the after each method.");
+    }
+
+    @Test
+    void firstMethod(){
+        System.out.println("This is the first Method.");
+    }
+
+
+
+    @Test
+    @DisplayName("US1234 - TC12 - This method is the second one.")
+    void secondMethod(){
+        System.out.println("This is the second Method.");
+    }
 }
--- src/test/java/junit5tests/ParameterizedTests.java
+++ src/test/java/junit5tests/ParameterizedTests.java
@@ -1,4 +1,20 @@
 package junit5tests;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class ParameterizedTests {
+    @ParameterizedTest(name = "Run: {index} - value: {arguments}")
+    @ValueSource(ints = {1, 5, 6})
+    void intValues(int theParam){
+        System.out.println("theParam = " + theParam);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"firstString", "secondString"})
+    void stringValues(String theParam){
+        System.out.println("the Param = " + theParam);
+    }
 }
