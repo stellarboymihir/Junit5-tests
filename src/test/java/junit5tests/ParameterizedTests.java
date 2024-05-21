3dedeb21964e97b89363c6c6796fc37b53458203
stellarboymihir <stellarboymihir00@gmail.com>
2024-05-21 16:37:44 +0530

    Parameterized Tests with @MethodSource

--- src/test/java/junit5tests/ParameterizedTests.java
+++ src/test/java/junit5tests/ParameterizedTests.java
@@ -1,10 +1,16 @@
 package junit5tests;
 
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ParameterizedTests {
     @ParameterizedTest(name = "Run: {index} - value: {arguments}")
     @ValueSource(ints = {1, 5, 6})
@@ -43,5 +49,59 @@ public class ParameterizedTests {
     void csvSource_StringWithDelimiter(String param1, String param2){
         System.out.println("param1: " + param1 + ", param2: " + param2);
     }
+
+    @ParameterizedTest
+    @CsvFileSource(files = {"src/test/resources/params/shoppinglist.csv", "src/test/resources/params/shoppingList2.csv"}, numLinesToSkip = 1)
+    void csvSource_StringIntDoubleStringString(String name, int price, Double quantity, String UOF, String provider){
+        System.out.println("The name: " + name + ", price: " + price + ", quantity: " + quantity + ", unit of measure: " + UOF + ", provider: " + provider);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = {"src/test/resources/params/shoppinglist3.csv"}, numLinesToSkip = 1, delimiterString = "__")
+    void csvSource_StringIntDoubleStringStringSpecificDelimiter(String name, int price, Double quantity, String UOF, String provider){
+        System.out.println("The name: " + name + ", price: " + price + ", quantity: " + quantity + ", unit of measure: " + UOF + ", provider: " + provider);
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "sourceString")
+    void methodSource_String(String param1){
+        System.out.println("param1: " + param1);
+    }
+
+    List<String> sourceString(){
+        return Arrays.asList("tomato", "carrot", "cabbage");
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "sourceStringAsSteam")
+    void methodSource_StringStream(String param1){
+        System.out.println("param1: " + param1);
+    }
+
+    Stream<String> sourceStringAsSteam(){
+//        processing
+        return Stream.of("beetroot", "apple", "pear");
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "sourceList_StringDouble")
+    void methodSource_StringDoubleList(String param1, double param2){
+        System.out.println("param1: " + param1 + ", param2: " + param2);
+    }
+
+    List<Arguments> sourceList_StringDouble(){
+//         processing
+        return Arrays.asList(arguments("tomato", 2.0),
+                arguments("carrot", 4.5), arguments("cabbage", 7.8));
+
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "junit5tests.ParamProvider#sourceStream_StringDouble")
+    void methodSource_StringDoubleStream(String param1, double param2){
+        System.out.println("param1: " + param1 + ", param2: " + param2);
+    }
+
+
 
 }
