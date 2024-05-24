c06a950d0ee9e43450df6ef9ca210f449e44d4bd
stellarboymihir <stellarboymihir00@gmail.com>
2024-05-24 15:28:17 +0530

    Assertions

--- src/test/java/junit5tests/AssertionsTest.java
+++ src/test/java/junit5tests/AssertionsTest.java
@@ -1,2 +1,56 @@
-package junit5tests;public class AssertionsTest {
+package junit5tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AssertionsTest {
+    @Test
+    void assertsEqualsTest(){
+        Assertions.assertEquals("firstString", "secondString",
+                "The String values are not equal.");
+
+
+    }
+
+    @Test
+    void assertEqualsListTest(){
+        List<String> actualValues = Arrays.asList("firstString",
+                "secondString", "thirdString");
+        List<String> expectedValues = Arrays.asList("firstString",
+                "secondString");
+        Assertions.assertEquals(expectedValues, actualValues);
+    }
+
+    @Test
+    void assertArraysEqualsTest(){
+        int[] expectedValues = {1, 2, 3};
+        int[] actualValues = {1,4, 3};
+       Assertions.assertArrayEquals(expectedValues, actualValues);
+    }
+    @Test
+    void assertTrueTest(){
+        Assertions.assertFalse(false);
+        Assertions.assertTrue(false, "This boolean condition did not evaluate to true");
+    }
+
+    @Test
+    void assertThrowsTest(){
+        Assertions.assertThrows(NullPointerException.class, null);
+    }
+
+    @Test
+    void assertAllTest(){
+        Assertions.assertAll(
+                () -> Assertions.assertEquals("firstString", "secondString",
+                        "The String values are not equal."),
+                () -> Assertions.assertThrows(NullPointerException.class, null),
+                () -> Assertions.assertTrue(false, "This boolean condition did not evaluate to true")
+
+
+        );
+    }
+
 }
